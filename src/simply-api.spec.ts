09882fcb39f6708564f881eb9cd6b32c036cef11
Romanchuk <rezety@gmail.com>
2018-06-29 11:35:28 +0700

    cleanup

--- src/simply-api.spec.ts
+++ src/simply-api.spec.ts
@@ -1,4 +1,4 @@
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+/*import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { SimplyApiModule, SimplyApiService } from '.';
@@ -66,3 +66,4 @@ describe('ApiModule', () => {
         expect(actualUsers).toEqual(expectedUsers);
       });
     });
+*/
--- src/utils.ts
+++ src/utils.ts
@@ -1,25 +1,26 @@
 export class QueryHelper {
-    /**
-     * Converts an object to a parametrised string.
-     * @param object
-     * @returns {string}
-     */
     static objectToParams(object): string {
-        return Object.keys(object).map((key) => object[key] instanceof Object ?
-            this.subObjectToParams(encodeURIComponent(key), object[key]) :
-            `${encodeURIComponent(key)}=${encodeURIComponent(object[key])}`
-        ).join('&');
+        const keys = Object.keys(object);
+        let result = [];
+        for (let key in keys) {
+            if (object[key]) {
+                result.push(object[key] instanceof Object ?
+                    this.subObjectToParams(encodeURIComponent(key), object[key]) :
+                    `${encodeURIComponent(key)}=${encodeURIComponent(object[key])}`);
+            }
+        }
+        return result.join('&');
     }
-
-    /**
-     * Converts a sub-object to a parametrised string.
-     * @param object
-     * @returns {string}
-     */
     static subObjectToParams(key, object): string {
-        return Object.keys(object).map((childKey) => object[childKey] instanceof Object ?
+        const keys = Object.keys(object);
+        let result = [];
+        for (let childKey in keys) {
+            if (object[childKey]) {
+                result.push(object[childKey] instanceof Object ?
                 this.subObjectToParams(`${key}[${encodeURIComponent(childKey)}]`, object[childKey]) :
-                `${key}[${encodeURIComponent(childKey)}]=${encodeURIComponent(object[childKey])}`
-        ).join('&');
+                `${key}[${encodeURIComponent(childKey)}]=${encodeURIComponent(object[childKey])}`);
+            }
+        }
+        return result.join('&');
     }
 }
